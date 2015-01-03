595616c59efa31af8afa1f6ff9703fe4be0a3bbf
herrhelms <dev@straw.io>
2015-01-02 23:35:14 -0300

    initial commit

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'herrhelms:meteor-leaflet-markercluster',
-  summary: 'L.MarkerCluster/MarkerClusterGroup by @danzel ported to meteor',
+  summary: 'L.MarkerClusterGroup by @danzel ported to meteor',
   version: '0.0.1',
   git: 'https://github.com/herrhelms/meteor-leaflet-markercluster.git'
 });
